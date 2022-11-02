df8c3c8039a9ac49f6c9d7a5e2b948673da992d4
Roman Minchenko <minchenkors@gmail.com>
2022-11-02 18:59:31 +0300

    add linter flake8

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 import sys
+from cli import welcome_user
+
 sys.path.insert(1, '/home/minchenkors/python-project-49/brain_games')
 
-from cli import welcome_user
 
 def main():
     print('Welcome to the Brain Games!')
     welcome_user()
-    
+
+
 if __name__ == '__main__':
     main()
