74e2877dae53d217e330af0d09e04184d4422f90
Roman Minchenko <minchenkors@gmail.com>
2022-11-04 23:57:48 +0300

    add progression game

---
+++ brain_games/games/progression_game.py
@@ -0,0 +1,29 @@
+from random import randint, choice
+from game_manager import GameManager
+
+
+class ProgressionGame(GameManager):
+
+    def __init__(self):
+        super().__init__()
+        self.rules = 'What number is missing in the progression?'
+
+    def generate_question(self):
+        progression_length = randint(5, 10)
+        missing_number_position = randint(0, progression_length - 1)
+        starting_number = randint(1, 50)
+        step = randint(5, 20)
+        progression = [starting_number + step * i for i in range(0, progression_length)]
+        self.right_answer = str(progression[missing_number_position])
+        progression[missing_number_position] = '..'
+        self.question = " ".join(map(str, progression))
+    
+    def make_game(self):
+        self.greet()
+        print(self.rules)
+        while self.round_number <= 2:
+            self.round_number += 1
+            self.generate_question()
+            self.make_round()
+            if self.check_answer_result == False:
+                break
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+sys.path.insert(1, '/home/minchenkors/python-project-49/brain_games/games')
+from progression_game import ProgressionGame
+
+
+def main():
+    my_game = ProgressionGame()
+    my_game.make_game()
+
+
+if __name__ == '__main__':
+    main()
